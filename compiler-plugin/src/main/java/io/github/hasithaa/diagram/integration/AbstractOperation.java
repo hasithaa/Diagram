e59cb24d21ac4e1d1070f0fd24eeda6dd295b418
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2024-02-06 06:56:35 +0530

    Add integration model
    Signed-off-by: Hasitha Aravinda <hasithaa@users.noreply.github.com>

---
+++ compiler-plugin/src/main/java/io/github/hasithaa/diagram/integration/AbstractOperation.java
@@ -0,0 +1,99 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.github.hasithaa.diagram.integration;
+
+import io.github.hasithaa.diagram.flowchart.BasicNode;
+import io.github.hasithaa.diagram.flowchart.Edge;
+import io.github.hasithaa.diagram.flowchart.FlowchartComponent;
+
+import java.util.List;
+
+public abstract class AbstractOperation implements Operation {
+
+    final private int id;
+    final private String description;
+    private AbstractOperation next, previous;
+    private FlowchartComponent flowchartComponent = null;
+    private List<Edge> flowchartEdges = null;
+
+    public AbstractOperation(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    @Override
+    public Operation nextOperation() {
+        return next;
+    }
+
+    @Override
+    public void setNextOperation(Operation nextOperation) {
+        this.next = (AbstractOperation) nextOperation;
+    }
+
+    @Override
+    public Operation previousOperation() {
+        return previous;
+    }
+
+    @Override
+    public void setPreviousOperation(Operation previousOperation) {
+        this.previous = (AbstractOperation) previousOperation;
+    }
+
+    @Override
+    public FlowchartComponent getFlowchartNode() {
+        if (flowchartComponent == null) {
+            new BasicNode(getNodeId(id), getDescription(description), getFlowchartNodeKind());
+        }
+        return flowchartComponent;
+    }
+
+    @Override
+    public List<Edge> getFlowchartEdges() {
+        if (flowchartEdges != null) {
+            return flowchartEdges;
+        }
+        if (previous == null && next == null) {
+            flowchartEdges = List.of();
+        } else if (previous == null) {
+            flowchartEdges = List.of(new Edge(getFlowchartNode(), next.getFlowchartNode()));
+        } else if (next == null) {
+            flowchartEdges = List.of(new Edge(previous.getFlowchartNode(), getFlowchartNode()));
+        } else {
+            flowchartEdges = List.of(new Edge(previous.getFlowchartNode(), getFlowchartNode()),
+                                     new Edge(getFlowchartNode(), next.getFlowchartNode()));
+        }
+        return flowchartEdges;
+    }
+
+    @Override
+    public String icon() {
+        return "⚙️";
+    }
+
+    @Override
+    public String getNodeId(int sequence) {
+        return "Node" + sequence;
+    }
+
+    @Override
+    public String getDescription(String description) {
+        return "[" + icon() + " " + getKind().name + "] " + description;
+    }
+}
---
+++ compiler-plugin/src/main/java/io/github/hasithaa/diagram/integration/templates/Clone.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package io.github.hasithaa.diagram.integration.templates;
+
+import io.github.hasithaa.diagram.flowchart.Edge;
+import io.github.hasithaa.diagram.flowchart.NodeKind;
+import io.github.hasithaa.diagram.integration.AbstractCompositeOutOperation;
+import io.github.hasithaa.diagram.integration.BBKind;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clone extends AbstractCompositeOutOperation {
+
+    final List<Sequence> sequences = new ArrayList<Sequence>();
+
+    public Clone(int id, String description) {
+        super(id, description);
+    }
+
+    @Override
+    public List<Edge> getFlowchartEdges() {
+        return super.getFlowchartEdges();
+    }
+
+    @Override
+    public NodeKind getFlowchartNodeKind() {
+        return NodeKind.SPECIAL;
+    }
+
+    @Override
+    public BBKind getKind() {
+        return BBKind.CLONE;
+    }
+
+    @Override
+    public String icon() {
+        return "⇶";
+    }
+
+}
